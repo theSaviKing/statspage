585d8a4121dc01cf15df7cda6ba333c4a333a10c
theSaviKing <tpaulkariuki@gmail.com>
2023-06-10 17:58:44 -0500

    added icons, worked on index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["'DM Sans'", ...defaultTheme.fontFamily.sans],
+                serif: ["'DM Serif Display'", ...defaultTheme.fontFamily.serif],
+                mono: ["'DM Mono'", ...defaultTheme.fontFamily.mono]
+            }
+        },
     },
     plugins: [require("daisyui")],
     daisyui: {
