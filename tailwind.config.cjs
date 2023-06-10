5ddfe7ed748db8b31b3ce50ef508d3dbbfbb0ba5
theSaviKing <tpaulkariuki@gmail.com>
2023-06-10 16:09:32 -0500

    begin new index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#00355D",
+                    secondary: "#377CFB",
+                    accent: "#FF671D",
+                    neutral: "#b3bbcc",
+                    "base-100": "#0D0D0D",
+                    info: "#3ABFF8",
+                    success: "#36D399",
+                    warning: "#FBBD23",
+                    error: "#F87272",
+                },
+            },
+        ],
+    },
+};
