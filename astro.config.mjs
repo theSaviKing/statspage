53ab5a073845971e195cfdda5d9a2e89ca77498f
theSaviKing <tpaulkariuki@gmail.com>
2023-06-18 21:07:22 -0500

    more deployment options

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,5 @@ export default defineConfig({
     integrations: [tailwind()],
     output: "server",
     adapter: netlify(),
+    site: "https://statspage.netlify.app/"
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly API_URL: string;
+}
