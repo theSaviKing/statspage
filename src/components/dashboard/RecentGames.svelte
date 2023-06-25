06764c0d3bb4a4d114b8ece66400f4024129033e
theSaviKing <tpaulkariuki@gmail.com>
2023-06-25 15:38:05 -0500

    added django cors support

---
+++ src/components/dashboard/RecentGames.svelte
@@ -0,0 +1,22 @@
+<script>
+    export const limit = 3;
+    var api_url = import.meta.env.PUBLIC_API_URL
+    api_url = "https://api.sampleapis.com/coffee/hot"
+    var game_data_url = `${api_url}/games/recent_games/?limit=${limit}&format=json`
+    const game_data = (async () => {
+        const data = await fetch(api_url)
+        return await data.json()
+    })()
+</script>
+
+{#await game_data}
+<div class="stat animate-pulse bg-base-200">
+    <div class="stat-value">Loading...</div>
+</div>
+{:then data}
+<div>{data}</div>
+{:catch error}
+<div class="stat">
+    <div>Sorry, there was an error:<br>{error}</div>
+</div>
+{/await}
