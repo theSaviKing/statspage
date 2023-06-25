b784b4c04be3de90e2acc1490342eaf44f3842c1
theSaviKing <tpaulkariuki@gmail.com>
2023-06-25 18:43:50 -0500

    worked out API problems

--- src/components/dashboard/RecentGames.svelte
+++ src/components/dashboard/RecentGames.svelte
@@ -1,22 +1,53 @@
 <script>
     export const limit = 3;
-    var api_url = import.meta.env.PUBLIC_API_URL
-    api_url = "https://api.sampleapis.com/coffee/hot"
-    var game_data_url = `${api_url}/games/recent_games/?limit=${limit}&format=json`
+    const game_data_url = new URL(
+        `/games/recent_games/?limit=${limit}&format=json`,
+        import.meta.env.PUBLIC_API_URL
+    );
     const game_data = (async () => {
-        const data = await fetch(api_url)
-        return await data.json()
-    })()
+        const data = await fetch(game_data_url.href);
+        return await data.json();
+    })();
+    const team_data_url = new URL(
+        `/teams/?format=json`,
+        import.meta.env.PUBLIC_API_URL
+    );
+    const team_data = (async () => {
+        const data = await fetch(team_data_url.href);
+        return await data.json();
+    })();
 </script>
 
-{#await game_data}
-<div class="stat animate-pulse bg-base-200">
-    <div class="stat-value">Loading...</div>
-</div>
-{:then data}
-<div>{data}</div>
+{#await team_data}
+    <div class="bg-base-200 p-6 rounded-xl animate-pulse">
+        Loading team data...
+    </div>
+{:then team_data}
+    {#await game_data}
+        <div class="p-6 bg-base-200 rounded-xl animate-pulse">
+            Loading game data...
+        </div>
+    {:then games}
+        <div class="flex flex-col gap-4">
+            {#each games as game}
+                <div class="stat bg-base-200 rounded-xl">
+                    <div class="stat-value">
+                        {team_data.find((team) => team.id === game.home_team)
+                            .short_name} vs. {team_data.find(
+                            (team) => team.id === game.away_team
+                        ).short_name}
+                    </div>
+                </div>
+            {/each}
+        </div>
+    {:catch error}
+        <div class="stat">
+            <div>Sorry, there was an error:<br />{error}</div>
+        </div>
+    {/await}
 {:catch error}
-<div class="stat">
-    <div>Sorry, there was an error:<br>{error}</div>
-</div>
+    <div class="stat bg-base-200 rounded-xl">
+        Sorry, there was an error loading team data:<br />
+        {error}
+    </div>
 {/await}
