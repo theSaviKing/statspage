2391159db5705e3f41116da0a20c507a5e9725c6
theSaviKing <tpaulkariuki@gmail.com>
2023-06-25 15:38:05 -0500

    more dashboard/stats API tooling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-    output: "server",
-    adapter: netlify(),
-    site: "https://statspage.netlify.app/"
+  integrations: [tailwind(), svelte()],
+  output: "server",
+  adapter: netlify(),
+  site: "https://statspage.netlify.app/"
 });
